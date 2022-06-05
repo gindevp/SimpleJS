9a8441bb406232697f3523e4e4d0c690b518282e
= <=>
2022-06-06 05:50:31 +0700

    thuchanh1

---
+++ b1.js
@@ -0,0 +1,18 @@
+var i=10;
+var f =20.5;
+var b=true;
+var s="ha noi"
+
+document.write(i)
+
+document.write(f)
+
+document.write(b)
+
+document.write(s)
+ a=prompt("nhạp a:" )
+b=prompt("mhap b:")
+if(a%b==0){
+    alert("a chia het b")
+}
+b
