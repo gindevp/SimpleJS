51723938b7fe2d962aff73fd16dafca9948257d0
= <=>
2022-06-06 13:13:56 +0700

    thuchanh6

--- b6.js
+++ b6.js
@@ -3,7 +3,16 @@ var Amount= document.getElementById("amount").value;
 var FormC =document.getElementById("form").value;
 var To=document.getElementById("to").value;
 var Result;
+if (FormC=="usd"&& To =="vnd"){
+    Result= "Result:"+ (Amount*23000)+"Đ";
+}else if(FormC=="vnd"&& To =="usd"){
+    Result="Result:"+ (Amount/23000)+"$";
+}else if(FormC=="usd"){
+    Result="Result:"+ Amount+"$"
+}else{
+    Result="Result"+Amount + "Đ"
+}
 
-
+document.getElementById("result").innerHTML= Result;
 
 }
