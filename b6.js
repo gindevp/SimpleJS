c8742ad6aeef6f01f7b691b15729ff3de78a19c7
= <=>
2022-06-06 12:22:41 +0700

    done

---
+++ b6.js
@@ -0,0 +1,9 @@
+function changeMoney(){
+var Amount= document.getElementById("amount").value;
+var FormC =document.getElementById("form").value;
+var To=document.getElementById("to").value;
+var Result;
+
+
+
+}
