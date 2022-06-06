31db0c6817605156bcc2f96d98dbc56fb7b57e32
= <=>
2022-06-06 14:56:24 +0700

    done

---
+++ bai7.js
@@ -0,0 +1,12 @@
+function Add(){
+let number1= document.getElementById("num1").value;
+let number2=document.getElementById("num2").value;
+let result= Number(number1)+ Number(number2);
+document.getElementById("result").innerHTML=result;
+}
+function Sub(){
+let number1= document.getElementById("num1").value;
+    let number2=document.getElementById("num2").value;
+    let result= Number(number1)- Number(number2);
+    document.getElementById("result").innerHTML=result;
+}
